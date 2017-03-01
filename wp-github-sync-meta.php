ca52f6c8c9c907333f1e21250ab496ad331d3534
zheyu <lite3@qq.com>
2017-03-01 21:18:16 +0800

    add tags

--- wp-github-sync-meta.php
+++ wp-github-sync-meta.php
@@ -9,6 +9,7 @@
  * License:      GPL2
  */
 
+// add tags and categories to github
 add_filter('wpghs_post_meta', function ($meta, $wpghs_post) {
     $tags = array();
     $list = wp_get_post_tags( $wpghs_post->post->ID );
@@ -30,3 +31,36 @@ add_filter('wpghs_post_meta', function ($meta, $wpghs_post) {
 
     return $meta;
 }, 10, 2);
+
+
+// github tags and categories to post
+add_filter('wpghs_pre_import_args', function ($args, $wpghs_post) {
+
+    $meta = $wpghs_post->get_meta();
+    echo "meta start\n";
+    var_dump($meta);
+    echo "meta end\n";
+    if (!empty($meta['tags'])) {
+        $args['tags_input'] = $meta['tags'];
+    }
+    // if (!empty($meta['categories'])) {
+    //     $args['post_category'] = $meta['categories'];
+    // }
+
+    return $args;
+}, 10, 2);
+
+
+// github meta to post
+add_filter('wpghs_pre_import_meta', function ($meta, $wpghs_post) {
+    unset($meta['tags']);
+    unset($meta['categories']);
+
+    // unset wordpress github sync meta
+    unset($meta['author']);
+    unset($meta['post_date']);
+    unset($meta['post_excerpt']);
+    unset($meta['permalink']);
+    return $meta;
+}, 10, 2);
+
